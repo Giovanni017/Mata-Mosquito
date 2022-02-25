a0aa9b89cee85ba55e76605025da7be31b0237ca
Giovanni017 <92633222+Giovanni017@users.noreply.github.com>
2022-02-25 11:18:59 -0300

    adicao do mosquito e posicoes randomicas

--- JS/jogo.js
+++ JS/jogo.js
@@ -1,11 +1,33 @@
+//dimensao
+
 var altura = 0
 var largura = 0
 
 function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
 
-     altura = window.innerHeight
-     largura = window.innerWidth
-
-     console.log(altura, largura)
+	console.log(largura, altura)
 }
-ajustaTamanhoPalcoJogo
+
+ajustaTamanhoPalcoJogo()
+
+//posicoes randomicas
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = '../Assets/mosca.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
